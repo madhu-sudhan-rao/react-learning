0db269f729022ac3df94d9f2fa3f3678a16db324
pawanscales <pawanpediredla@gmail.com>
2025-06-12 21:32:09 +0530

    added password generator, copy button logic

--- src/problems/password-generator/PasswordGenerator.tsx
+++ src/problems/password-generator/PasswordGenerator.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PasswordGenerator.css";
 
 function PasswordGenerator() {
+  const [password, setPassword] = useState("");
+
+  const generatePassword = (length = 12) => {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const number = "0123456789";
+    const symbol = "!@#$%^&*()_+{}[]<>?/|";
+
+    const getRandom = (set: string) =>
+      set[Math.floor(Math.random() * set.length)];
+
+    let pwd = [
+      getRandom(upper),
+      getRandom(lower),
+      getRandom(number),
+      getRandom(symbol),
+    ];
+
+    const allChars = upper + lower + number + symbol;
+
+    while (pwd.length < length) {
+      pwd.push(getRandom(allChars));
+    }
+
+    setPassword(pwd.sort(() => Math.random() - 0.5).join(""));
+  };
+
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(password);
+  }
+
   return (
     <div className="password-generator-container">
       <header>
@@ -10,13 +42,13 @@ function PasswordGenerator() {
       </header>
       <main className="body">
         <section className="generator-section">
-          <div className="password">PASSWORD</div>
+          <div className="password">{password || 'Generate Password'}</div>
           <div className="action-buttons">
             <div className="copy-button">
-              <button>Copy</button>
+              <button onClick={copyPassword}>Copy</button>
             </div>
             <div className="regenerate-button">
-                <button>Regenerate</button>
+              <button onClick={() => generatePassword()}>Regenerate</button>
             </div>
           </div>
         </section>
